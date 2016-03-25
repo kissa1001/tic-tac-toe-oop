65374ae3ea028b015df377325a6554465d406a25
kissa1001 <kissa101295@gmail.com>
2016-03-25 18:50:51 -0400

    winner function edited

--- app.js
+++ app.js
@@ -1,71 +1,81 @@
-Player = function(name,sign){
+var Player = function(name, sign){
   this.name = name;
   this.sign = sign;
 };
-var player1 = new Player('lina','x');
-var player2 = new Player('capi','o');
 
-TicTacToe = function(players){
-  this.board = [[null,null,null],
-                [null,null,null],
-                [null,null,null]
-               ];
-  this.players = [player1,player2];
-  this.filledSquare = 0;
+var TicTacToe = function(players){
+    this.board = [
+        [null, null, null],
+        [null, null, null],
+        [null, null, null]
+    ];
+    this.players = players;
+    this.filledSquares = 0;
 };
-TicTacToe.prototype.move= function(players,posX,posY){
-  if(this.validate(posX,posY)){
-    if(this.filledSquare % 2 === 0){
-      this.board[posX][posY]=player1.sign;
-      console.log(player1.name + ' chose '+ [posX,posY]);
-      ttt.winner();
+
+TicTacToe.prototype.currentPlayer = function() {
+    var index = this.filledSquares % 2;
+    return this.players[index]; // returns Player object
+};
+
+TicTacToe.prototype.playerBySign = function(sign) {
+    // return the Player that has the sign
+    for (var i=0; i < this.players.length; i++) {
+        if (this.players[i].sign === sign) {
+            return this.players[i].name;
+        }
     }
-    else{
-      this.board[posX][posY]=player2.sign;
-      console.log(player2.name + ' chose '+ [posX,posY]);
-      ttt.winner();
-    }
-    this.filledSquare++;
+};
+
+TicTacToe.prototype.move= function(name, posX, posY) {
+  if (this.validatePosition(posX, posY) && this.validatePlayer(name)){
+      this.board[posX][posY] = this.currentPlayer().sign;
+      console.log(name + ' chose '+ [posX, posY]);
+      this.filledSquares++;
+      console.log(this.winner());
   }
 };
-TicTacToe.prototype.validate = function(posX,posY){
-  if(this.board[posX][posY] === null){
-    return true;
-  }
-  else{
-    console.log('This cell is not available!');
-  }
+
+
+TicTacToe.prototype.validatePosition = function(posX,posY){
+  if (this.board[posX][posY] === null) { return true; }
+  else { console.log('This cell is not available!'); }
 };
-TicTacToe.prototype.winner= function(value){
-  this.value = 'x' || 'o';
-  if(this.board[0][0] === this.value && this.board[0][1] ===this.value && this.board[0][2] === this.value){
-    console.log(this.value + ' won');
-  }
-  else if(this.board[1][0] === this.value && this.board[1][1] === this.value && this.board[1][2] === this.value){
-    console.log(this.value + ' won');
-  }
-  else if(this.board[2][0] === this.value && this.board[2][1] === this.value && this.board[2][2]=== this.value){
-    console.log(this.value + ' won');
-  }
-  else if(this.board[0][0] === this.value && this.board[2][0] === this.value && this.board[2][0]=== this.value){
-    console.log(this.value + ' won');
-  }
-  else if(this.board[0][1] === this.value && this.board[1][1] === this.value && this.board[2][1] === this.value){
-    console.log(this.value + ' won');
-  }
-  else if(this.board[0][2] === this.value && this.board[1][2] === this.value && this.board[2][2] === this.value){
-    console.log(this.value + ' won');
-  }
-  else if(this.board[0][0] === this.value && this.board[1][1] === this.value && this.board[2][2]=== this.value){
-    console.log(this.this.value + ' won');
-  }
-  else if(this.board[0][2] === this.value && this.board[1][1] === this.value && this.board[2][0]=== this.value){
-    console.log(this.value + ' won');
-  }
+
+TicTacToe.prototype.validatePlayer = function(name) {
+    if (this.currentPlayer().name === name) { return true; }
+    else { console.log("This is not " + name + "'s turn."); }
 };
-var ttt = new TicTacToe();
-ttt.move('lina',0,0);
-ttt.move('capi',2,0);
-ttt.move('lina',0,1);
-ttt.move('capi',2,1);
-ttt.move('lina',0,2);
+
+TicTacToe.prototype.winner = function() {
+    var winner;
+    
+    this.board.forEach(function(line){
+        if (line[0] !== null && line[0] === line[1] && line[1] === line[2]) {
+            winner = line[0];
+        }
+    });
+    
+    var board = this.board;
+    [0, 1, 2].forEach(function(index){
+        var column = board.map(function(line){ return line[index]; });
+        if (column[0] !== null && column[0] == column[1] && column[1] == column[2]) { winner = column[0]; }
+    });
+    
+    if (this.board[0][0] !== null && this.board[0][0] == this.board[1][1] && this.board[1][1] == this.board[2][2]) { winner = this.board[0][0]; }
+    
+    if (this.board[2][0] !== null && this.board[2][0] == this.board[1][1] && this.board[1][1] == this.board[0][2]) { winner = this.board[2][0]; }
+    
+    return this.playerBySign(winner);
+};
+
+var ttt = new TicTacToe([
+    new Player('lina', 'x'),
+    new Player('capi', 'o')
+]);
+
+ttt.move('lina', 0, 0);
+ttt.move('capi', 0, 2);
+ttt.move('lina', 1, 0);
+ttt.move('capi', 1, 1);
+ttt.move('lina', 2, 0);
