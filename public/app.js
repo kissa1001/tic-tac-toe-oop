b9644de748bd49a258b32614cbc187cf1ec72ca3
kissa1001 <kissa101295@gmail.com>
2016-03-31 16:31:39 -0400

    added factory

--- public/app.js
+++ public/app.js
@@ -1,5 +1,4 @@
 $(function() {
-	var socket=io();
 	var input = $('.msgInput');
     var messages = $('#messages');
     var nickname;
@@ -19,32 +18,32 @@ $(function() {
         $("#chat").addClass("thin");
     });
 
-    //Add message
-    var addMessage = function(message) {
-        messages.append('<div>' + message + '</div>');
-    };
+    // //Add message
+    // var addMessage = function(message) {
+    //     messages.append('<div>' + message + '</div>');
+    // };
 
-    //User is typing
-    $("input").on("keyup", function (event) {
-        socket.emit("sender", {
-            nickname: nickname
-        });
-    });
-    socket.on("sender", function (data) {
-        $("#status").html(data.nickname + " is typing");
-        setTimeout(function () {
-            $("#status").html('');
-        }, 3000);
-    });
+    // //User is typing
+    // $("input").on("keyup", function (event) {
+    //     socket.emit("sender", {
+    //         nickname: nickname
+    //     });
+    // });
+    // socket.on("sender", function (data) {
+    //     $("#status").html(data.nickname + " is typing");
+    //     setTimeout(function () {
+    //         $("#status").html('');
+    //     }, 3000);
+    // });
 
-    input.on('keydown', function(event) {
-        if (event.keyCode != 13) {
-            return;
-        }
+    // input.on('keydown', function(event) {
+    //     if (event.keyCode != 13) {
+    //         return;
+    //     }
 
-        var message = input.val();
-        socket.emit('message', message);
-        input.val('');
-    });
-    socket.on('message',addMessage)
+    //     var message = input.val();
+    //     socket.emit('message', message);
+    //     input.val('');
+    // });
+    // socket.on('message',addMessage)
 });
--- public/game.js
+++ public/game.js
@@ -1,28 +1,25 @@
 var app = angular.module('game',[])
-    .controller('loginCtrl', function($rootScope, $scope){
-        if(!$rootScope.socket){
-            $rootScope.socket = io();
-        }
+    .factory('socket', function(){
+        return io();
+    })
+    .controller('loginCtrl',['$scope', '$rootScope', 'socket', function($scope,$rootScope , socket){
         $scope.$on('player1', function(){
             $rootScope.player = new Player($scope.name, $scope.sign);
-            $rootScope.socket.emit('join', $scope.name, $scope.sign);
+            socket.emit('join', $scope.name, $scope.sign);
             $rootScope.loggedIn = true;
         })
-    })
-    .controller('gameCtrl', function($rootScope, $scope){
-        if(!$rootScope.socket){
-            $rootScope.socket = io();
-        }
+    }])
+    .controller('gameCtrl', ['$scope', '$rootScope', 'socket', function($scope,$rootScope , socket){
         $rootScope.$on('player1', function(){
             $scope.ttt = new TicTacToe([
                 $rootScope.player,
                 new Player('Robot', 'O')
             ]);
         })
-        $rootScope.socket.on('join', function(){
+        socket.on('join', function(){
             console.log('Hello');
         })
         $scope.move = function(X,Y){
             $scope.ttt.move($rootScope.player.name, X, Y);
         }
-    });
+    }]);
